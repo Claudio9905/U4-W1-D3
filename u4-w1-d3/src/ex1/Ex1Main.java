fd9e47ceb1e9d2f6a0941b196ce811575521938a
Claudio9905 <cloudpost10@gmail.com>
2025-09-10 17:26:19 +0200

    Update File

--- u4-w1-d3/src/ex1/Ex1Main.java
+++ u4-w1-d3/src/ex1/Ex1Main.java
@@ -9,19 +9,15 @@ import static ex1.entities.Rettangolo.stampaDueRettangoli;
 public class Ex1Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double alt1 = 0;
-        double l1 = 0;
-        double alt2 = 0;
-        double l2 = 0;
-
+        
         System.out.println("Inserisci l'altezza del primo rettangolo: ");
-        alt1 = scanner.nextDouble();
+        double alt1 = scanner.nextDouble();
         System.out.println("Inserisci la larghezza del primo rettangolo: ");
-        l1 = scanner.nextDouble();
+        double l1 = scanner.nextDouble();
         System.out.println("Inserisci l'altezza del secondo rettangolo: ");
-        alt2 = scanner.nextDouble();
+        double alt2 = scanner.nextDouble();
         System.out.println("Inserisci la larghezza del secondo rettangolo: ");
-        l2 = scanner.nextDouble();
+        double l2 = scanner.nextDouble();
 
         Rettangolo rettangolo1 = new Rettangolo(alt1,l1);
         Rettangolo rettangolo2 = new Rettangolo(alt2,l2);
